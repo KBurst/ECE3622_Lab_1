ee268ea9c9fa8860450f004718e06ebf49aa5146
KingBurst <tlcherry189@gmail.com>
2019-02-13 23:12:55 -0500

    First commit

---
+++ Lab_1.sdk/.sdk/launch_scripts/performance_analysis/performance_analysis_using_debug_lab1.elf_on_local.tcl
@@ -0,0 +1,16 @@
+connect -url tcp:127.0.0.1:3121
+source C:/Zynq_Book/Lab_1/Lab_1.sdk/Lab_1_wrapper_hw_platform_0/ps7_init.tcl
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent Zybo 210279A42751A"} -index 0
+loadhw -hw C:/Zynq_Book/Lab_1/Lab_1.sdk/Lab_1_wrapper_hw_platform_0/system.hdf -mem-ranges [list {0x40000000 0xbfffffff}]
+configparams force-mem-access 1
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent Zybo 210279A42751A"} -index 0
+stop
+ps7_init
+ps7_post_config
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zybo 210279A42751A"} -index 0
+rst -processor
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zybo 210279A42751A"} -index 0
+dow C:/Zynq_Book/Lab_1/Lab_1.sdk/Lab1/Debug/Lab1.elf
+configparams force-mem-access 0
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zybo 210279A42751A"} -index 0
+con
